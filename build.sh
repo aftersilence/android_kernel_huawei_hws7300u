15c4557348cde9bf1b0773aaf63f62c6d2db79aa
Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
2012-03-29 17:23:11 -0600

    msm: kgsl: Allocate physical pages instead of using vmalloc
    Replace vmalloc allocation with physical page allocation. For most
    allocations we do not need a kernel virual address. vmalloc uses up
    the kernel virtual address space. By replacing vmalloc with physical
    page alloction and mapping that allocation to kernel space only
    when it is required prevents the kgsl driver from using unnecessary
    vmalloc virtual space.
    
    CRs-Fixed: 346921
    
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    (cherry picked from commit 8cb835b656ae2c3faa79af4f374563d344b46d4a)
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_sharedmem.c
    
    Change-Id: I88e1eb282cc4822970811728d00b1096188f64e9
    Signed-off-by: Ram Kumar Chakravarthy Chebathini <rcheba@codeaurora.org>
    (cherry picked from commit d5fa912f599a71223496d2ff97ecd5040b95baaf)
    
    Signed-off-by: Zhoulu Luo <zluo@codeaurora.org>
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl.c
    	drivers/gpu/msm/kgsl_sharedmem.c
    	drivers/gpu/msm/kgsl_sharedmem.h
    
    Conflicts:
    
    	build.sh
    	drivers/gpu/msm/kgsl.c
    	drivers/gpu/msm/kgsl_sharedmem.c
    	drivers/gpu/msm/kgsl_sharedmem.h

---
+++ build.sh
@@ -0,0 +1,21 @@
+export KBUILD_BUILD_VERSION="3.0.38"
+export KBUILD_BUILD_USER="Mediapad EX"
+export CROSS_COMPILE="/home/zyr3x/android/kernel/android_prebuilt_toolchains/arm-eabi-4.4.3/bin/arm-eabi-"
+
+ROOTFS_PATH="/home/ubu/mediapad/ramdisk"
+ramdisk_dir=/home/ubu/mediapad/ramdisk/ramdisk.gz
+CMDLINE="console=console=ttyHSL0,115200,n8 androidboot.hardware=hws7300u vmalloc=578M kgsl.ptcount=16"
+BASE="0x40300000"
+
+# Do not modify below this line
+
+NB_CPU=`grep processor /proc/cpuinfo | wc -l`
+
+let NB_CPU+=1
+
+CONFIG='mediapad_defconfig'
+
+#make ARCH=arm $CONFIG
+
+make ARCH=arm -j$NB_CPU CROSS_COMPILE=$CROSS_COMPILE
+
