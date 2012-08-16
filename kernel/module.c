dc489e92db9ce0e14cdc92d5cb6017fa9d983e20
Bart594 <ubu@ubuntu.ubuntu-domain>
2012-08-16 18:00:54 +0200

    module.c: kernel: make an exception in module loading

--- kernel/module.c
+++ kernel/module.c
@@ -1022,6 +1022,9 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+        if(!strncmp("dhd", mod->name, 3))
+        return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
