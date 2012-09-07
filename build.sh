f2d5123afe4b74e2cf1d6f3de091a3ab5351b646
aftersilence <913704@mail.ru>
2012-09-07 14:57:27 +0400

    Build script

--- build.sh
+++ build.sh
@@ -1,21 +1,13 @@
-export KBUILD_BUILD_VERSION="3.0.38"
-export KBUILD_BUILD_USER="Mediapad EX"
-export CROSS_COMPILE="/home/zyr3x/android/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
-
-ROOTFS_PATH="/home/ubu/mediapad/ramdisk"
-ramdisk_dir=/home/ubu/mediapad/ramdisk/ramdisk.gz
 CMDLINE="console=console=ttyHSL0,115200,n8 androidboot.hardware=hws7300u vmalloc=578M kgsl.ptcount=16"
 BASE="0x40300000"
+ramdisk_dir=/home/aftersilence/ramdisk.cpio.gz
 
-# Do not modify below this line
+make -j4
 
-NB_CPU=`grep processor /proc/cpuinfo | wc -l`
+cp arch/arm/boot/zImage .
 
-let NB_CPU+=1
+find -name '*.ko' -exec cp -av {} lib/modules/ \;
 
-CONFIG='mediapad_defconfig'
+# Make boot.img
 
-make ARCH=arm $CONFIG
-
-make ARCH=arm -j$NB_CPU CROSS_COMPILE=$CROSS_COMPILE
-
+./mkbootimg --cmdline "$CMDLINE" --base $BASE --kernel zImage --ramdisk $ramdisk_dir -o boot_new.img
