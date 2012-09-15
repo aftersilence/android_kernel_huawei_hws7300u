3b92bf9a13ba1b8d9ca75f7961f66d33214791f2
aftersilence <913704@mail.ru>
2012-09-15 15:54:14 +0400

    msm:vdec: Correct filled length of the output buffer
    Filled length of the output buffer at FBD for videos
    with frame height less than 96 was incorrect. With
    this change, we get the proper filled length.
    
    CRs-fixed: 395179
    Change-Id: I12d60ef653225b0bf2f0088e515cac19a19ed80a
    Signed-off-by: Deepak Verma <dverma@codeaurora.org>
    (cherry picked from commit f01274b904a4899b13c64f35f2479a1080bfb2c4)
    
    Conflicts:
    
    	drivers/video/msm/vidc/1080p/ddl/vcd_ddl_properties.c

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_properties.c
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_properties.c
@@ -1873,7 +1873,20 @@ u32 ddl_set_default_decoder_buffer_req(struct ddl_decoder_data *decoder,
 		output_buf_req = &decoder->actual_output_buf_req;
 		input_buf_req = &decoder->actual_input_buf_req;
 		min_dpb = decoder->min_dpb_num;
-		y_cb_cr_size = decoder->y_cb_cr_size;
+		if ((decoder->buf_format.buffer_format ==
+			VCD_BUFFER_FORMAT_TILE_4x2) &&
+			(frame_size->height < MDP_MIN_TILE_HEIGHT)) {
+			frame_size->height = MDP_MIN_TILE_HEIGHT;
+			ddl_calculate_stride(frame_size,
+				!decoder->progressive_only);
+			y_cb_cr_size = ddl_get_yuv_buffer_size(
+				frame_size,
+				&decoder->buf_format,
+				(!decoder->progressive_only),
+				decoder->hdr.decoding, NULL);
+			decoder->y_cb_cr_size = y_cb_cr_size;
+		} else
+			y_cb_cr_size = decoder->y_cb_cr_size;
 	}
 	memset(output_buf_req, 0,
 		sizeof(struct vcd_buffer_requirement));
