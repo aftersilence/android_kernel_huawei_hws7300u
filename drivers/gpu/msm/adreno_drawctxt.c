72f78e83c32fa47ab62faeab36cff3f54c1fe539
aftersilence <913704@mail.ru>
2012-10-04 15:09:40 +0400

    Fix building

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -217,7 +217,7 @@ void adreno_drawctxt_destroy(struct kgsl_device *device,
 	}
 
 	if (device->state != KGSL_STATE_HUNG)
-		adreno_idle(device);
+		adreno_idle(device, KGSL_TIMEOUT_DEFAULT);
 
 	if (adreno_is_a20x(adreno_dev) && adreno_dev->drawctxt_active)
 		kgsl_setstate(&device->mmu, adreno_dev->drawctxt_active->id,
