7265d15f0ec762107b78b2f1de781a85fd4d125f
aftersilence <913704@mail.ru>
2012-10-06 21:37:58 +0400

    Revert " Ezekeel's Fsync Control"
    This reverts commit 0a0f9ebb9b9324b245510411f7ccba1102bdb8c7.

--- fs/sync.c
+++ fs/sync.c
@@ -139,10 +139,6 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
 	file = fget_light(fd, &fput_needed);
 	if (!file)
 		return -EBADF;
@@ -171,11 +167,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
-
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
 
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
@@ -209,22 +200,15 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
-        return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
+	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
 
 static int do_fsync(unsigned int fd, int datasync)
 {
-        struct file *file;
+	struct file *file;
 	int ret = -EBADF;
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
+
 	file = fget(fd);
 	if (file) {
 		ret = vfs_fsync(file, datasync);
@@ -235,19 +219,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
-        return do_fsync(fd, 0);
+	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
 	return do_fsync(fd, 1);
 }
 
@@ -261,10 +237,6 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
@@ -329,10 +301,6 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	int fput_needed;
 	umode_t i_mode;
 
-#ifdef CONFIG_FSYNC_CONTROL
-       if (!fsynccontrol_fsync_enabled)
-       return 0;
-#endif
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
 		goto out;
