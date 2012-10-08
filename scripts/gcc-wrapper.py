bb2d9a08e1a16e5459a4eaf75019c1383d37efe5
aftersilence <913704@mail.ru>
2012-10-08 15:08:41 +0400

    gcc-4.7.0 has started throwing these warnings when building cifs.ko.
    CC [M] fs/cifs/cifssmb.o
    fs/cifs/cifssmb.c: In function ‘CIFSSMBSetCIFSACL’:
    fs/cifs/cifssmb.c:3905:9: warning: array subscript is above array bounds [-Warray-bounds]
    fs/cifs/cifssmb.c: In function ‘CIFSSMBSetFileInfo’:
    fs/cifs/cifssmb.c:5711:8: warning: array subscript is above array bounds [-Warray-bounds]
    fs/cifs/cifssmb.c: In function ‘CIFSSMBUnixSetFileInfo’:
    fs/cifs/cifssmb.c:6001:25: warning: array subscript is above array bounds [-Warray-bounds]
    
    This patch cleans up the code a bit by using the offsetof macro instead
    of the funky "&pSMB->hdr.Protocol" construct.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -46,8 +46,6 @@ allowed_warnings = set([
     "vx6953.c:3124",
     "dma-mapping.c:238",
     "dma-mapping.c:284",
-    "cifssmb.c:5318",
-    "cifssmb.c:5608",
  ])
 
 # Capture the name of the object file, can find it.
