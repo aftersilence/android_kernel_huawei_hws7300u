d4a0beb2245e3a893b3bdbc0c33a52e211249b16
aftersilence <913704@mail.ru>
2013-01-22 13:06:17 +0400

    msm_fb: display: Flush pipe registers before staging the pipe
    Flush pipe registers before staging the pipe for the Mixer with
    pull mode panel interfaces. This fixes the IOMMU page fault/underrun
    caused by pipe fetching previously unmapped buffers due to missing flush
    calls
    
    CRs-Fixed: 356484
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    (cherry picked from commit cfd004fa326a6dc4c7e80da707248b5bddaa39b8)
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c
    
    Change-Id: I830ef580bf2eb51d83396f1b177ea530fe68eb2f
    Signed-off-by: Ram Kumar Chakravarthy Chebathini <rcheba@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2925,6 +2925,11 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		mdp4_overlay_rgb_setup(pipe);	/* rgb pipe */
 	}
 
+	if ((ctrl->panel_mode & MDP4_PANEL_DTV) ||
+		(ctrl->panel_mode & MDP4_PANEL_LCDC) ||
+		(ctrl->panel_mode & MDP4_PANEL_DSI_VIDEO))
+		mdp4_overlay_reg_flush(pipe, 0);
+
 	mdp4_mixer_stage_up(pipe);
 
 	if (pipe->mixer_num == MDP4_MIXER2) {
@@ -2939,7 +2944,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		ctrl->mixer1_played++;
 		/* external interface */
 		if (ctrl->panel_mode & MDP4_PANEL_DTV) {
-			mdp4_overlay_reg_flush(pipe, 0);
 			mdp4_overlay_dtv_start();
 			mdp4_overlay_dtv_ov_done_push(mfd, pipe);
 			if (!mfd->use_ov1_blt)
@@ -2950,7 +2954,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		/* primary interface */
 		ctrl->mixer0_played++;
 		if (ctrl->panel_mode & MDP4_PANEL_LCDC) {
-                mdp4_overlay_reg_flush(pipe, 0);
 			mdp4_overlay_lcdc_start();
 			mdp4_overlay_lcdc_vsync_push(mfd, pipe);
                         if (!mfd->use_ov0_blt &&
@@ -2959,7 +2962,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		}
 #ifdef CONFIG_FB_MSM_MIPI_DSI
 		else if (ctrl->panel_mode & MDP4_PANEL_DSI_VIDEO) {
-                        mdp4_overlay_reg_flush(pipe, 0);
 			mdp4_overlay_dsi_video_start();
 			mdp4_overlay_dsi_video_vsync_push(mfd, pipe);
                         if (!mfd->use_ov0_blt &&
